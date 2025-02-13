c6d046b2a802dec78ea3a504a3f4113b23b50de5
George <g_ekhaesomi@yahoo.com>
2025-02-13 20:05:07 +0100

    fix: updated deploy.yml

--- api/routes/books.py
+++ api/routes/books.py
@@ -51,11 +51,12 @@ async def get_books() -> OrderedDict[int, Book]:
 async def get_book(book_id: int) -> JSONResponse:
 
     book = db.get_book(book_id)
-    
+
     if book is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="404 Not Found")
-    
+
     return book
+
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
@@ -63,6 +64,7 @@ async def update_book(book_id: int, book: Book) -> Book:
         status_code=status.HTTP_200_OK,
         content=db.update_book(book_id, book).model_dump(),
     )
+
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
